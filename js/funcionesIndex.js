14e7677435ca3db9b89ea8537786564f2b645a0b
angelesgomez <gomezangeles.lab@gmail.com>
2023-05-31 23:26:04 -0300

    Actualizacion de informacion y estilos - v.1.1.0

--- js/funcionesIndex.js
+++ js/funcionesIndex.js
@@ -1,19 +1,11 @@
 const navBoton = document.querySelector(".nav__boton")
 const navMenu = document.querySelector(".nav-lista") 
+const menu = document.querySelector('header')
 
 navBoton.addEventListener("click", () => {
     navMenu.classList.toggle("nav-lista--visible");
 });
-const menu = document.querySelector('header')
 
-    window.addEventListener('scroll', function() {
-        menu.classList.toggle('header--active', window.scrollY >0)
-    });
-
-const acordeon = document.getElementsByClassName('proyecto-article');
-
-for(let i=0; i<acordeon.length;i++){
-    acordeon[i].addEventListener('click', function(){
-        this.classList.toggle('proy-active')
-    })
-}
+window.addEventListener('scroll', function() {
+    menu.classList.toggle('header--active', window.scrollY >0)
+});
